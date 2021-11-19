42e7e44da3074b270708c1544926f2d2f8735aff
KateM3d <ekaterina8medvedeva@gmail.com>
2021-11-19 18:58:41 -0500

    initial commit

---
+++ week28/state/src/components/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+import "./Card.scss";
+
+function Card(props) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  let handleSelection = () => {
+    setIsSelected(!isSelected);
+  };
+
+  return (
+    <div
+      onClick={handleSelection}
+      className={"cardBody" + (props.selected === true ? " circle" : "")}
+    >
+      <div className={`cardTop_${props.cost}`}>Unlimited {props.cost}</div>
+      <div className={`cardCenter_${props.cost}`}>
+        $ <p className="number">{props.cost}</p> /per month
+      </div>
+      <div className="cardSpeed">{props.speed}</div>
+      <div className="cardTraffic">
+        Included traffic<br></br> volume is unlimited
+      </div>
+    </div>
+  );
+}
+
+export default Card;
